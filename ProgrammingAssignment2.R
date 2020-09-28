9cfaf5e9936b7ea203a7a9d7781f38964d886ab6
JuanPablo0618 <juan0618pablo@gmail.com>
2020-09-27 21:55:27 -0500

    Hi

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,25 +1,33 @@
+
+##Crea un objeto de matriz especial que puede almacenar en caché su inverso
 makeCacheMatrix <- function( m = matrix() ) {
   
+  ##Iniciar la propiedad inversa
   i <- NULL
   
+  ##Metodo envio de matriz
   set <- function( matrix ) {
     m <<- matrix
     i <<- NULL
   }
   
+  ##Metodo para obtener la matriz
   get <- function() {
     m
   }
   
+  ##Envio de la matriz inversa
   setInverse <- function(inverse) {
     i <<- inverse
   }
-
+  
+  ##Obtener la matriz inversa
   getInverse <- function() {
 
     i
   }
   
+  ##Lista de los metodos
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
@@ -37,11 +45,15 @@ cacheSolve <- function(x, ...) {
     return(m)
   }
   
+  ##Obtener la matriz del propio objeto 
   data <- x$get()
   
+  ##Calculo de la matriz usando multiplicion de matriz
   m <- solve(data) %*% data
   
+  ##Envio inverso del objeto
   x$setInverse(m)
-
+  
+  ## Devuelve la matriz
   m
 }
